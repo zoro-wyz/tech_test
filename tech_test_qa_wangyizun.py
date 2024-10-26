4ef86d861a2fd436042e45f454769b437c7f3957
zoro-wyz <45930198+zoro-wyz@users.noreply.github.com>
2024-10-26 18:12:13 +0800

    Update tech_test_qa_wangyizun.py

--- tech_test_qa_wangyizun.py
+++ tech_test_qa_wangyizun.py
@@ -3,7 +3,7 @@ import psycopg2
 # 1. qa脚本连接数据库的信息具有隐私性，已经从脚本内容中移除，运行前需要补充。连接信息在笔试题文档中。
 # 2. 具有意外数值问题，发现 open_price 的值有效小数位没有遵循 digits 的规范。脚本中展示了部分样例。
 # 3. 具有数据重复问题，users 表中共有1000条数据，去重后有666条数据。trades 表去重前后数据都为100000条。
-# 4. 具有数据不完整问题，trades 表与 users 表进行连接时，数据为36067条，有部分 trade 数据没有对应 user。
+# 4. 具有数据不完整问题，trades 表与 users 表进行连接时，数据为8047条，有部分 trade 数据没有对应 user。
 # 5. 具有边缘问题，open_time 和 close_time 完全一致的数据有36条，需要跟业务部门确认该情况是否正常。
 db_params = {
     'dbname': '',  
@@ -45,7 +45,7 @@ try:
         print(f"user_count: {user_count}, distinct_user_count: {distinct_user_count}, trade_count: {trade_count}, distinct_trade_count:{distinct_trade_count}")
 
     # 检查数据完整性
-    cursor.execute("SELECT count(*) AS trade_with_user_count FROM trades LEFT JOIN users ON users.login_hash = trades.login_hash AND users.server_hash = trades.server_hash WHERE users.login_hash is NOT null AND users.server_hash is NOT null")
+    cursor.execute("SELECT COUNT(*) AS trade_with_user_count FROM trades t LEFT JOIN(SELECT DISTINCT login_hash, server_hash, country_hash, currency, enable FROM users) uu ON t.login_hash = uu.login_hash AND t.server_hash = uu.server_hash WHERE uu.login_hash IS NOT NULL AND uu.server_hash IS NOT NULL")
     results = cursor.fetchall()
     for result in results:
         trade_with_user_count = result
